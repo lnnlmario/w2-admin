15049dd949e13224b1fed48e50ebf08f8c7f21cf
lnnlmario <lnnlmario@163.com>
2024-02-20 22:43:37 +0800

    引入unplugin-vue-components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
-
 const msg = ref('home page.')
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,10 @@ export default defineConfig({
       eslintrc: {
         enabled: true
       }
+    }),
+    Components({
+      // resolvers: [NaiveUiResolver()],
+      dts: true
     })
   ],
   resolve: {
